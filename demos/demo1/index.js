554aa13ba433acf73e7f1dc4cc4376a9d0b10c82
唐王瑶 <tangwangyao@hualala.com>
2023-03-13 18:40:01 +0800

    feat: output tensor object

---
+++ demos/demo1/index.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: 唐王瑶
+ * @Email：tangwangyao@hualala.com
+ * @Date: 2023-03-13 18:03:40
+ * @LastEditors: 唐王瑶
+ * @Description: 示例1的入口文件
+ * @FilePath: /twy-ai/demos/demo1/index.js
+ */
+import * as tf from '@tensorflow/tfjs';
+const t0 = tf.tensor(1);
+t0.print();
+console.log(t0,'t0');
