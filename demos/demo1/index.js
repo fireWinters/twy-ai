1e14dee1102951bc9cc9456946651e4ff5557ec6
唐王瑶 <tangwangyao@hualala.com>
2023-03-14 15:13:04 +0800

    feat: 新增一层神经元对数据的处理

--- demos/demo1/index.js
+++ demos/demo1/index.js
@@ -7,6 +7,26 @@
  * @FilePath: /twy-ai/demos/demo1/index.js
  */
 import * as tf from '@tensorflow/tfjs';
-const t0 = tf.tensor(1);
-t0.print();
-console.log(t0,'t0');
+// const t0 = tf.tensor(1);
+// t0.print();
+// console.log(t0,'t0');
+const input = [1, 2, 3, 4];
+//第一层神经元的权重
+const w = [
+  [1, 2, 3, 4],
+  [2, 3, 4, 5],
+  [3, 4, 5, 6],
+  [4, 5, 6, 7],
+];
+const output = [0, 0, 0, 0];
+
+// //循环权重
+// for (let i = 0; i < w.length; i++) {
+//   for (let j = 0; j < input.length; j++) {
+//     //1 * [1, 2, 3, 4]遍历 + 2 * [1, 2, 3, 4]遍历
+//     output[i] += input[j] * w[i][j];
+//   }
+// }
+// console.log('output: ', output);
+
+tf.tensor(w).dot(tf.tensor(input)).print();
